b48f052b717352b5ead736757eae338b10c4b423
Benny Ng <ngbenny@mfcgd.com>
2020-03-23 15:52:37 +0800

    chore - fix test

--- src/transactions/transactions.controller.spec.ts
+++ src/transactions/transactions.controller.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionsController } from './transactions.controller';
+import { TransactionsService } from './transactions.service';
 
 describe('Transactions Controller', () => {
   let controller: TransactionsController;
 
+  const mockTransactionModel = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: 'TransactionModel',
+          useValue: mockTransactionModel,
+        },
+        TransactionsService,
+      ],
       controllers: [TransactionsController],
     }).compile();
 
-    controller = module.get<TransactionsController>(TransactionsController);
+    controller = module.get(TransactionsController);
   });
 
   it('should be defined', () => {
--- src/transactions/transactions.service.spec.ts
+++ src/transactions/transactions.service.spec.ts
@@ -4,9 +4,17 @@ import { TransactionsService } from './transactions.service';
 describe('TransactionsService', () => {
   let service: TransactionsService;
 
+  const mockTransactionModel = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionsService],
+      providers: [
+        {
+          provide: 'TransactionModel',
+          useValue: mockTransactionModel,
+        },
+        TransactionsService,
+      ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
