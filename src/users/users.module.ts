81a8d0c03bef15deac24cce90f5eb5ff8668aea6
Benny Ng <ngbenny@mfcgd.com>
2020-03-25 01:13:35 +0800

    publish user created event after signup

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
 import { UsersController } from './users.controller';
+import { RmqClientModule } from 'src/rmq-client/rmq-client.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    RmqClientModule,
+  ],
   providers: [UsersService],
   exports: [UsersService],
   controllers: [UsersController],
